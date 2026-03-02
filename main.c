54139ada30d1e7442d3cf5ae83db1410c81c5ed8
xvader0007 <60202009+xvader0007@users.noreply.github.com>
2026-03-03 02:58:58 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 10;
+
+    printf("Hello, World!\n");
+
+    if(a == 10)
+    {
+        a++;
+        printf("%s", "Arman, privet!");
+    }
+
+    return 0;
+}
