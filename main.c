ee084ee199016d1ba6d1607234a1e1626b6d5c6b
xvader0007 <60202009+xvader0007@users.noreply.github.com>
2026-03-06 02:01:00 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+#include <math.h>
+#include "complex.c"
 
 int main(void)
 {
-    int a = 10;
-
-    printf("Hello, World!\n");
-
-    if(a == 10)
-    {
-        a++;
-        printf("%s", "Arman, privet!");
-    }
+    Complex a, b;
+    a.real_part = 1.23456789;
+    a.imag_part = 2.12345678;
+    b.real_part = 3.14;
+    b.imag_part = -2.71;
+    complex_print(a);
+    complex_print(b);
 
     return 0;
 }
